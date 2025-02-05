9d524731662520bcb393f28aaea44ffe4d789dc5
logank <logan.aker1989@gmail.com>
2025-02-05 10:44:43 -0500

    Restructured main function; moved many functions into draw() and calculate() functions within main()

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-import math
-import numpy
 from rich import print
 
 import chronology
@@ -10,43 +8,56 @@ THETA = "\u0398"
 ORIGIN = (10, 10)
 
 
-def get_latitude() -> float:
-    """Get user-input of latitude to use to calculate angles
-
-    Returns:
-        float: The provided latitude, in radians
-    """
-    l = float(
-        input(
-            "\nPlease enter the latitude for which you would like to receive calculations (must be decimal format): \n"
-        )
+def get_coordinates():
+    coords = input(
+        "Enter latitude and longitude for desired sundial location (decimal format): \n"
     )
-    latitude = math.radians(l)  # must convert to radians before calculations
-    print(f"\nLatitude: {l}{DEGREES}\n")
-    return latitude
+    lat, long = coords.split(" ")
+    latitude = float(lat.strip(","))
+    longitude = float(long)
+    return (latitude, longitude)
 
 
-if __name__ == "__main__":
+def draw(latitude, angle_list):
     template = Draw(ORIGIN, 15)
     template.create_circle()
     template.draw_equatorial()
     template.draw_meridian()
-    list_of_angles = []
-    latitude = get_latitude()
-    chronometer = chronology.Chronos(latitude)
-    for time in chronometer.times:
-        round_time = chronometer.get_round_time(time)
-        formatted_time = chronometer.format_time(round_time)
-        tan_theta = chronometer.calculate_tan_theta(time, chronometer.latitude)
-        result = max(-90, min(90, round(tan_theta, 2)))
-        entry = str(f"{formatted_time} = {result}{DEGREES}")
-        print(entry)
-        list_of_angles.append(result)
-    template.hour_line(list_of_angles)
-    template.label_latitude(numpy.rad2deg(latitude))
-    with open("angles.txt", "a") as f:
-        for angle in list_of_angles:
-            f.write(str(angle))
-    eot_correction = chronometer.get_data_table()
-    print(eot_correction)
+    template.create_circle()
+    template.draw_equatorial()
+    template.hour_line(angle_list)
+    template.label_latitude(round(latitude, 2))
     template.draw()
+    return
+
+
+def calculate(latitude, longitude):
+    angle_list = []
+    fmt_angle_list = []
+    chronos = chronology.Chronos(latitude, longitude)
+    sundial_tz = chronos.get_timezone()
+    utc_offset = chronos.get_utc_offset(sundial_tz)
+    central_meridian = chronos.find_central_meridian(utc_offset)
+    # TODO: dial_tilt = chronos.calculate_dial_tilt(central_meridian)
+    # TODO: dial_rotation = chronos.calculate_dial_rotation(central_meridian)
+    for time in chronos.times:
+        round_time = chronos.get_round_time(time)
+        formatted_time = chronos.format_time(round_time)
+        tan_theta = chronos.calculate_tan_theta(time, chronos.latitude)
+        result = max(-90, min(90, round(tan_theta, 2)))
+        fmt_result = str(f"{formatted_time} = {result}{DEGREES}")
+        angle_list.append(result)
+        fmt_angle_list.append(fmt_result)
+    data_table = chronos.get_data_table()
+    return fmt_angle_list, angle_list, data_table
+
+
+if __name__ == "__main__":
+    latitude, longitude = get_coordinates()
+    formatted_angle_list, angle_list, data_table = calculate(latitude, longitude)
+    draw(latitude, angle_list)
+    eot_correction = data_table
+    print(eot_correction)
+    print(latitude, longitude)
+    for item in formatted_angle_list:
+        print(item)
